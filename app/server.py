fa0f0ee3a8b5b27b9385c8e64936eeecd5c58e16
tarun gupta <rajeshguptamsk12@gmail.com>
2026-02-02 10:43:55 +0530

    created post route and created shell script for rerun the app

--- app/server.py
+++ app/server.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, UploadFile
+from uuid import uuid4
 app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {"status": "healthy"}
+    return {"status": "container is healthy and fit"}
+
+@app.post("/upload")
+def upload_file(file: UploadFile):
+
+    id = uuid4()
+
+    file_path = f"/mnt/uploads/{id}/{file.filename}"
+    
